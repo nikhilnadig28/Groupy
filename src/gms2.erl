1d6ee613129d38b5fd040ff7f11ac3ba1e1d6fca
Nikhil Nadig <nikhilnadig28@gmail.com>
2017-10-02 15:14:26 +0200

    gms2 file added

--- src/gms2.erl
+++ src/gms2.erl
@@ -1,24 +1,33 @@
--module (gms1).
--export ([leader/4, slave/5]).
+-module (gms2).
+-export ([function/arity]).
 
 start(Id) ->
+    Rnd = random:uniform(1000),
     Self = self(),
-    {ok, spawn_link(fun()-> init(Id, Self) end)}.
+    {ok, spawn_link(fun()-> init(Id, Rnd, Self) end)}.
 
-init(Id, Master) ->
+init(Id, Rnd, Master) ->
+    random:seed(Rnd, Rnd, Rnd),
     leader(Id, Master, [], [Master]).
 
 start(Id, Grp) ->
+    Rnd = random:uniform(1000),
     Self = self(),
-    {ok, spawn_link(fun()-> init(Id, Grp, Self) end)}.
+    {ok, spawn_link(fun()-> init(Id, Grp, Rnd, Self) end)}.
 
-init(Id, Grp, Master) ->
+init(Id, Grp, Rnd, Master) ->
+	random:seed(Rnd, Rnd, Rnd),
+    
     Self = self(),
     Grp ! {join, Master, Self},
+    
     receive
         {view, [Leader|Slaves], Group} ->
             Master ! {view, Group},
             slave(Id, Master, Leader, Slaves, Group)
+
+    after ?timeout ->
+	    Master ! {error, "no reply from leader"}
 end.    
 
 leader(Id, Master, Slaves, Group) ->
@@ -27,6 +36,7 @@ leader(Id, Master, Slaves, Group) ->
             bcast(Id, {msg, Msg}, Slaves),
             Master ! Msg,
             leader(Id, Master, Slaves, Group);
+        
         {join, Wrk, Peer} -> %a message, from a peer or the master, that is a request from a node to join the group. The message contains both the process identifier of the application layer, Wrk, and the process identifier of its group process.
             Slaves2 = lists:append(Slaves, [Peer]),
             Group2 = lists:append(Group, [Wrk]),
@@ -51,7 +61,33 @@ slave(Id, Master, Leader, Slaves, Group) ->
         {view, [Leader|Slaves2], Group2} -> %  a multicasted view from the leader. A view is delivered to the master process.
             Master ! {view, Group2}
             slave(Id, Master, Leader, Slaves2, Group2);
+        {'DOWN', _Ref, process, Leader, _Reason} ->
+	    	election(Id, Master, Slaves, Group);
         stop ->
-ok end.
+			ok 
+	end.
 
 
+election(Id, Master, Slaves, [_|Group]) ->
+    Self = self(),
+    case Slaves of
+        [Self|Rest] ->
+            bcast(Id, {view, Slaves, Group}, Rest),
+            Master ! {view, Group},
+            leader(Id, Master, Rest, Group);
+        [Leader|Rest] ->
+            erlang:monitor(process, Leader),
+            slave(Id, Master, Leader, Rest, Group)
+end.
+
+bcast(Id, Msg, Nodes) ->
+    lists:foreach(fun(Node) -> Node ! Msg, crash(Id) end, Nodes).
+
+crash(Id) ->
+    case random:uniform(?arghh) of
+        ?arghh ->
+            io:format("leader ~w: crash~n", [Id]),
+            exit(no_luck);
+		_ ->
+			ok
+end.
